0b8b78e5237713ae7de374fd62301eaaa644de43
Circle CI <circleci@circleci.org>
2019-11-19 22:55:37 +0000

    [ci skip] Circle CI deploy

---
+++ precache-manifest.af3b321fae7f7a199b15c45a25519c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f627b481cc7a3b77f24f6199202eca0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e0c375ae7cb06be3e8",
+    "url": "/static/css/2.d5534402.chunk.css"
+  },
+  {
+    "revision": "b1396ffa55c2cfed02bb",
+    "url": "/static/css/main.dfc1a628.chunk.css"
+  },
+  {
+    "revision": "76e0c375ae7cb06be3e8",
+    "url": "/static/js/2.4f56500a.chunk.js"
+  },
+  {
+    "revision": "b1396ffa55c2cfed02bb",
+    "url": "/static/js/main.0d9b837a.chunk.js"
+  },
+  {
+    "revision": "33ae3d35fc4ca9b8b1c6",
+    "url": "/static/js/runtime~main.2e421be2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nuclear-monitor/precache-manifest.eb50315dba83b13a2389baaec3a9b7c0.js"
+  "/precache-manifest.af3b321fae7f7a199b15c45a25519c1f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nuclear-monitor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
